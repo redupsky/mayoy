9e426f1847c3b32ff698532916f834eb2779ab4d
Alexey Golovnya <alexey.golovnya@gmail.com>
2016-09-24 10:56:41 +0600

    Formatting

--- src/Juxta.elm
+++ src/Juxta.elm
@@ -6,88 +6,106 @@ import Html.Events exposing (onInput, onClick, on, keyCode)
 import Html.App as App
 import Json.Decode
 
-textAreaId = "editor"
+
+textAreaId =
+    "editor"
+
 
 main =
-  App.program
-    { view = view
-    , init = init
-    , update = update
-    , subscriptions = subscriptions
-    }
+    App.program
+        { view = view
+        , init = init
+        , update = update
+        , subscriptions = subscriptions
+        }
 
 
 
 -- Subscriptions
 
+
 port runCodemirror : String -> Cmd msg
+
 
 port requestTextFromCodemirror : String -> Cmd msg
 
+
 port connect : ConnectionParameters -> Cmd msg
 
-port connectionEstablished : ( ConnectionParameters -> msg ) -> Sub msg
 
-port connectionFailed : ( String -> msg ) -> Sub msg
+port connectionEstablished : (ConnectionParameters -> msg) -> Sub msg
 
-port pressRunInCodemirror : ( String -> msg ) -> Sub msg
 
-port receiveTextFromCodemirror : ( String -> msg ) -> Sub msg
+port connectionFailed : (String -> msg) -> Sub msg
+
+
+port pressRunInCodemirror : (String -> msg) -> Sub msg
+
+
+port receiveTextFromCodemirror : (String -> msg) -> Sub msg
 
 
 subscriptions model =
-  Sub.batch
-    [ connectionEstablished ConnectionEstablished
-    , connectionFailed ConnectionFailed
-    , pressRunInCodemirror ( \_ -> TryToRun )
-    , receiveTextFromCodemirror ReceiveTextAndRun
-    ]
+    Sub.batch
+        [ connectionEstablished ConnectionEstablished
+        , connectionFailed ConnectionFailed
+        , pressRunInCodemirror (\_ -> TryToRun)
+        , receiveTextFromCodemirror ReceiveTextAndRun
+        ]
 
 
 
 -- Model
 
+
 type alias ConnectionParameters =
-  { hostAndPort : ( String, Int )
-  , user : String
-  , password : String
-  , database : Maybe String
-  }
+    { hostAndPort : ( String, Int )
+    , user : String
+    , password : String
+    , database : Maybe String
+    }
 
-type alias ConnectionError = (String, Int)
 
-type alias ThreadId = Int
+type alias ConnectionError =
+    ( String, Int )
+
+
+type alias ThreadId =
+    Int
 
 
 type Connection
-  = NoConnection
-  | Connecting ConnectionParameters
-  | Failed ConnectionError
-  | Established ( ConnectionParameters, ThreadId )
+    = NoConnection
+    | Connecting ConnectionParameters
+    | Failed ConnectionError
+    | Established ( ConnectionParameters, ThreadId )
 
 
-localhost = ConnectionParameters ("localhost", 3306) "root" "" Nothing
+localhost =
+    ConnectionParameters ( "localhost", 3306 ) "root" "" Nothing
 
 
 type alias Model =
-  { errors: List String
-  , connection : Connection
-  , text : String
-  , results : List Query
-  , status : String
-  }
+    { errors : List String
+    , connection : Connection
+    , text : String
+    , results : List Query
+    , status : String
+    }
 
 
-type alias Query = ( String, Maybe Result )
+type alias Query =
+    ( String, Maybe Result )
 
 
-type Result = List String
+type Result
+    = List String
 
 
 init =
-  ( Model [] NoConnection "select * from mysql.tables where name like '%mysql%'" []  ""
-  , Cmd.none
-  )
+    ( Model [] NoConnection "select * from mysql.tables where name like '%mysql%'" [] ""
+    , Cmd.none
+    )
 
 
 
@@ -95,34 +113,33 @@ init =
 
 
 type Message
-  = Connect ConnectionParameters
-  | ConnectionFailed String
-  | ConnectionEstablished ConnectionParameters
-  | TryToRun
-  | ReceiveTextAndRun String
-  | RunCommand
+    = Connect ConnectionParameters
+    | ConnectionFailed String
+    | ConnectionEstablished ConnectionParameters
+    | TryToRun
+    | ReceiveTextAndRun String
+    | RunCommand
 
 
-update msg model  =
-  case msg of
-    Connect connection ->
-      ( { model | errors = [], connection = Connecting connection }, connect connection )
+update msg model =
+    case msg of
+        Connect connection ->
+            ( { model | errors = [], connection = Connecting connection }, connect connection )
 
-    ConnectionFailed error ->
-      ( { model | errors = [error], connection = Failed (error, 0) }, Cmd.none )
+        ConnectionFailed error ->
+            ( { model | errors = [ error ], connection = Failed ( error, 0 ) }, Cmd.none )
 
-    ConnectionEstablished connection ->
-      ( { model | connection = Established (connection, 0) }, runCodemirror textAreaId )
+        ConnectionEstablished connection ->
+            ( { model | connection = Established ( connection, 0 ) }, runCodemirror textAreaId )
 
-    ReceiveTextAndRun text ->
-      update RunCommand { model | text = text}
+        ReceiveTextAndRun text ->
+            update RunCommand { model | text = text }
 
-    RunCommand ->
-      ( { model | status = "Run: " ++ model.text }, Cmd.none )
+        RunCommand ->
+            ( { model | status = "Run: " ++ model.text }, Cmd.none )
 
-    TryToRun ->
-      ( model, requestTextFromCodemirror "" )
-
+        TryToRun ->
+            ( model, requestTextFromCodemirror "" )
 
 
 
@@ -130,98 +147,100 @@ update msg model  =
 
 
 view model =
-  case model.connection of
-    Established _ ->
-      viewWorkspace model
+    case model.connection of
+        Established _ ->
+            viewWorkspace model
 
-    _ ->
-      viewLogin model
+        _ ->
+            viewLogin model
 
 
 viewLogin model =
-  let
-    isConnecting =
-      case model.connection of
-        Connecting _ ->
-          True
-        _ ->
-          False
-  in
-    div []
-      [ viewErrors model.errors
-      , button [ onClick <| Connect <| localhost, disabled isConnecting ] [ text "Connect.." ]
-      ]
+    let
+        isConnecting =
+            case model.connection of
+                Connecting _ ->
+                    True
+
+                _ ->
+                    False
+    in
+        div []
+            [ viewErrors model.errors
+            , button [ onClick <| Connect <| localhost, disabled isConnecting ] [ text "Connect.." ]
+            ]
 
 
 viewWorkspace model =
-  main' []
-    [ viewErrors model.errors
-    , viewHeader model
-    , viewQuery model.text
-    , viewResultTable model.results
-    , viewStatus model.status
-    ]
+    main' []
+        [ viewErrors model.errors
+        , viewHeader model
+        , viewQuery model.text
+        , viewResultTable model.results
+        , viewStatus model.status
+        ]
 
 
 viewErrors errors =
-    div [] ( List.map text errors )
+    div [] (List.map text errors)
 
 
 viewHeader model =
-  let
-    connectionName = case model.connection of
-      Established ({user, hostAndPort}, _) ->
-        let
-          host =
-            fst hostAndPort
-          portNumber =
-            snd hostAndPort
-        in
-          user ++ "@" ++ host ++ ":" ++ (toString portNumber)
+    let
+        connectionName =
+            case model.connection of
+                Established ( { user, hostAndPort }, _ ) ->
+                    let
+                        host =
+                            fst hostAndPort
 
-      _ ->
-        ""
-  in
-    header [ class "header" ]
-        [ div [ class "header-left" ]
-            [ select []
-                [ option [] [ text connectionName ]
+                        portNumber =
+                            snd hostAndPort
+                    in
+                        user ++ "@" ++ host ++ ":" ++ (toString portNumber)
+
+                _ ->
+                    ""
+    in
+        header [ class "header" ]
+            [ div [ class "header-left" ]
+                [ select []
+                    [ option [] [ text connectionName ]
+                    ]
+                ]
+            , div [ class "header-right" ]
+                [ button [ class "query-run-button", onClick TryToRun ] [ text "Run" ]
                 ]
             ]
-        , div [ class "header-right" ]
-            [ button [ class "query-run-button", onClick TryToRun ] [ text "Run" ]
-            ]
-        ]
 
 
 viewQuery queries =
-  div [ class "query" ]
-    [ textarea [ class "query-text", id textAreaId ] [ text queries ]
-    ]
+    div [ class "query" ]
+        [ textarea [ class "query-text", id textAreaId ] [ text queries ]
+        ]
 
 
 viewResultTable results =
-  if List.length results > 0 then
-    table [class "results-table"]
-      [ tbody []
-        [ tr []
-          [ td []
-            [ text "Qq"
+    if List.length results > 0 then
+        table [ class "results-table" ]
+            [ tbody []
+                [ tr []
+                    [ td []
+                        [ text "Qq"
+                        ]
+                    , td []
+                        [ text "zZ.."
+                        ]
+                    ]
+                ]
             ]
-          , td []
-            [ text "zZ.."
+    else
+        div []
+            [ text "Not results yet"
             ]
-          ]
-        ]
-      ]
-
-  else
-    div []
-      [ text "Not results yet"
-      ]
 
 
 viewStatus status =
-  div [ class "status" ]
-    [ text status
-    ]
+    div [ class "status" ]
+        [ text status
+        ]
