045dd5fb575aefa41096ab0daf73001d86267cc2
Alexey Golovnya <alexey.golovnya@gmail.com>
2016-10-29 10:51:43 +0600

    Styles for connect form

--- src/Juxta/Connect/View.elm
+++ src/Juxta/Connect/View.elm
@@ -1,8 +1,8 @@
 module Juxta.Connect.View exposing (view)
 
-import Html exposing (div, text, input)
+import Html exposing (div, text, form, label, input)
 import Html.Attributes exposing (class, disabled, type', name, value, placeholder)
-import Html.Events exposing (onClick, onInput)
+import Html.Events exposing (onClick, onInput, onSubmit)
 import Juxta.Model exposing (Connection(Connecting), localhost, defaultPort)
 import Juxta.Connect.Message exposing (Message(Connect, ChangeFormHost, ChangeFormPort, ChangeFormUser, ChangeFormPassword))
 import Juxta.Component.ButtonWithIndicator exposing (buttonWithIndicator, rightOrNo)
@@ -30,20 +30,58 @@ viewConnect { connection, form } =
                 _ ->
                     False
     in
-        div []
-            [ div [ class "connect-form" ]
-                [ div []
-                    [ input [ type' "text", name "host", value form.host, onInput ChangeFormHost ] []
+        Html.form [ class "connect-form", onSubmit <| Connect <| formToConnectionParameters form ]
+            [ div [ class "connect-form-item" ]
+                [ label [ class "connect-form-label" ] [ text "Host:" ]
+                , input
+                    [ type' "text"
+                    , class "connect-form-input _host"
+                    , name "host"
+                    , value form.host
+                    , onInput ChangeFormHost
                     ]
-                , div []
-                    [ input [ type' "text", name "port", value form.portNumber, placeholder <| toString defaultPort, onInput ChangeFormPort ] []
+                    []
+                ]
+            , div [ class "connect-form-item" ]
+                [ label [ class "connect-form-label" ] [ text "Port:" ]
+                , input
+                    [ type' "text"
+                    , class "connect-form-input _port"
+                    , name "port"
+                    , value form.portNumber
+                    , placeholder <| toString defaultPort
+                    , onInput ChangeFormPort
                     ]
-                , div []
-                    [ input [ type' "text", name "user", value form.user, onInput ChangeFormUser ] []
+                    []
+                ]
+            , div [ class "connect-form-item" ]
+                [ label [ class "connect-form-label" ] [ text "User:" ]
+                , input
+                    [ type' "text"
+                    , class "connect-form-input _port"
+                    , name "user"
+                    , value form.user
+                    , onInput ChangeFormUser
                     ]
-                , div []
-                    [ input [ type' "password", name "password", value form.password, onInput ChangeFormPassword ] []
+                    []
+                ]
+            , div [ class "connect-form-item" ]
+                [ label [ class "connect-form-label" ] [ text "Password:" ]
+                , input
+                    [ type' "password"
+                    , class "connect-form-input _port"
+                    , name "password"
+                    , value form.password
+                    , onInput ChangeFormPassword
                     ]
-                , buttonWithIndicator [ disabled isConnecting, onClick <| Connect <| formToConnectionParameters form ] [ text "Connect.." ] (rightOrNo isConnecting)
+                    []
+                ]
+            , div [ class "connect-form-item _connect" ]
+                [ buttonWithIndicator
+                    [ disabled isConnecting
+                    , onClick <| Connect <| formToConnectionParameters form
+                    ]
+                    [ text "Connect.." ]
+                    (rightOrNo isConnecting)
                 ]
             ]
