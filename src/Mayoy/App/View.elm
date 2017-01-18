3c13583b63493478f1c007396932c75b03bf5b61
Alexey Golovnya <alexey.golovnya@gmail.com>
2017-01-18 08:03:57 +0700

    Add type annotations to Mayoy.App.View

--- src/Mayoy/App/View.elm
+++ src/Mayoy/App/View.elm
@@ -4,9 +4,11 @@ import Mayoy.App.Message exposing (Message(ConnectMessage, WorkspaceMessage))
 import Mayoy.App.Model exposing (Model(ConnectModel, WorkspaceModel))
 import Mayoy.Connect.View as Connect
 import Mayoy.Workspace.View as Workspace
+import Html
 import Html.App
 
 
+view : Model -> Html.Html Message
 view model =
     case model of
         ConnectModel model ->
